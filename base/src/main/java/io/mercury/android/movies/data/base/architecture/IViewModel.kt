d9881a09882c2addb0172b976f8eb02d85bda4b6
Derek Panger <dpanger13@gmail.com>
2019-01-12 21:23:45 -0600

    Add new interfaces from template

---
+++ base/src/main/java/io/mercury/android/movies/data/base/architecture/IViewModel.kt
@@ -0,0 +1,14 @@
+package io.mercury.android.movies.data.base.architecture
+
+/**
+ * Marker interface that serves documentation purposes for the view model pattern.
+ *
+ * A view model is an observable container for a single piece of data (whether that data is a
+ * composition of other view models/data or just a single piece of data). It is technically part of
+ * the middle layer in an Android architecture but can also be part of the data layer if the point
+ * of entry/point of abstraction needs to be higher. It manages the emission of said data as well as
+ * any other relevant information (ie, whether "loading" or not) to any observers. A view model
+ * should not contain references to the observers, but rather just emit data and relevant
+ * information to them.
+ */
+interface IViewModel
