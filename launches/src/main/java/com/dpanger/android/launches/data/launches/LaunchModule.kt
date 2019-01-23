9d912872bce629b77c5fcec7339069e8fa3aa93b
Derek Panger <dpanger13@gmail.com>
2019-01-23 13:13:41 -0600

    Update module and documentation

--- launches/src/main/java/com/dpanger/android/launches/data/launches/LaunchModule.kt
+++ launches/src/main/java/com/dpanger/android/launches/data/launches/LaunchModule.kt
@@ -5,7 +5,7 @@ import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import toothpick.config.Module
 
-class MovieModule : Module() {
+class LaunchModule : Module() {
 
     init {
         val retrofit = Retrofit.Builder()
--- launches/src/main/java/com/dpanger/android/launches/data/launches/package-info.java
+++ launches/src/main/java/com/dpanger/android/launches/data/launches/package-info.java
@@ -1,8 +1,8 @@
 /**
- * Module for movie information.
+ * Module for launch information.
  *
- * The module contains {@link com.dpanger.android.launches.data.launches.ILaunchRepository} which provides
- * movie information. It can be injected using Toothpick and the provided
- * {@link toothpick.config.Module}, {@link com.dpanger.android.launches.data.launches.MovieModule}.
+ * The module contains {@link com.dpanger.android.launches.data.launches.ILaunchRepository} which
+ * provides launch information. It can be injected using Toothpick and the provided
+ * {@link toothpick.config.Module}, {@link com.dpanger.android.launches.data.launches.LaunchModule}.
  */
 package com.dpanger.android.launches.data.launches;
