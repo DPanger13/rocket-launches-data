1367406e3711ed0cd8ea32951672987e9629a81d
Derek Panger <dpanger13@gmail.com>
2019-01-26 13:06:37 -0600

    Add logging

--- launches/src/main/java/com/dpanger/android/launches/data/launches/BackendSource.kt
+++ launches/src/main/java/com/dpanger/android/launches/data/launches/BackendSource.kt
@@ -2,15 +2,21 @@ package com.dpanger.android.launches.data.launches
 
 import io.reactivex.Single
 import io.reactivex.schedulers.Schedulers
+import timber.log.Timber
 import javax.inject.Inject
 
 internal open class BackendSource @Inject constructor(private val api: LaunchApi) {
 
     open fun getUpcomingLaunches(offset: Int? = null): Single<LaunchApi.PagedLaunchSummaryDto> {
+        Timber.d("Fetching upcoming launches with offset: $offset")
+
         return api.getUpcomingLaunches(offset).subscribeOn(Schedulers.io())
     }
 
-    open fun getLaunch(id: Int): Single<LaunchApi.PagedLaunchDto> =
-            api.getLaunch(LaunchApi.Mode.VERBOSE.mode, id).subscribeOn(Schedulers.io())
+    open fun getLaunch(id: Int): Single<LaunchApi.PagedLaunchDto> {
+        Timber.d("Fetching launch with ID: $id")
+
+        return api.getLaunch(LaunchApi.Mode.VERBOSE.mode, id).subscribeOn(Schedulers.io())
+    }
 
 }
